42f519f5ee4df987c29a8c0c1e00fe226734bed1
Lukáš Zaoral <lukaszaoral@outlook.com>
2019-05-03 22:31:58 +0200

    Add integration test

---
+++ smpc_test.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+cd build
+
+for i in {1..1000}; do
+    printf "TEST ${i}: "
+    
+    if ! yes | ./smpc_rsa client generate > /dev/null; then
+        continue
+        #exit 1
+    fi
+
+    if ! ./smpc_rsa client sign > /dev/null; then
+        continue
+        #exit 1
+    fi
+
+    if ! yes | ./smpc_rsa server generate > /dev/null; then
+        continue
+        #exit 1
+    fi
+
+    if ! ./smpc_rsa server sign > /dev/null; then
+        continue
+        #exit 1
+    fi
+
+    if ! ./smpc_rsa server verify > /dev/null; then
+        continue
+        #exit 1
+    fi
+
+    printf "\x1b[1;32mOK\x1b[0m\n"
+done;
